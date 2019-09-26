91d0e2f51de55759bd5742a49b43b41d55e149b3
Lior Chamla <lchamla@gmail.com>
2019-09-27 01:48:56 +0200

    mise en place du cache

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(Stopwatch $stopwatch)
+    public function index(Stopwatch $stopwatch, CacheInterface $cache)
     {
         $stopwatch->start('calcul-long');
 
         // On imagine un calcul ou un traitement long
-        $resultatCalcul = $this->fonctionQuiPrendDuTemps();
+        // $resultatCalcul = $this->fonctionQuiPrendDuTemps();
+        $resultatCalcul = $cache->get('resultat-calcul-long', function (ItemInterface $item) {
+            // Quand on voudrait que le cache se renouvelle après une minute par exemple
+            //$item->expiresAfter(60);
+
+            return $this->fonctionQuiPrendDuTemps();
+        });
 
         $stopwatch->stop('calcul-long');
 
